de8f376f2fd965e1b14d9eba3cefc836dc2643c7
Bob (Duchess) Maidens <bob.maidens@avalara.com>
2019-08-29 14:29:00 -0400

    Adding output to failures

--- src/Phpmig/Adapter/PDO/TransactionalSqlPgsql.php
+++ src/Phpmig/Adapter/PDO/TransactionalSqlPgsql.php
@@ -12,39 +12,48 @@ class TransactionalSqlPgsql extends SqlPgsql {
 
     public function execute(Migration $migration, $direction) {
 
-        $runInTransaction = True; //$migration->runInTransaction();
+        $runInTransaction = true; 
         $container = $migration->getContainer();
         $pdo_counter = $container['db'];
 
-        if ($runInTransaction === True) {
+        if ($runInTransaction === true) {
             $pdo_counter->beginTransaction();
         }
-        $successfulTransaction = True;
+        $successfulTransaction = true;
         $exception = null;
         $failedReason = null;
         try {
-            if (parent::execute($migration, $direction) === False) {
+            if (parent::execute($migration, $direction) === false) {
                 $failedReason = "Adapter failed to run the migration.";
-                $successfulTransaction = False;
+                $successfulTransaction = false;
             }
 
-            if ($runInTransaction === True) {
+            if ($runInTransaction === true) {
                 $successfulTransaction = $pdo_counter->commit();
-                if ($successfulTransaction === False) {
+                if ($successfulTransaction === false) {
                     $failedReason = "Adapter failed to commit.";
                 }
             }
         } catch (\PDOException $e) {
             $exception = $e;
-            $successfulTransaction = False;
-            if ($runInTransaction === True) {
+            $successfulTransaction = false;
+            if ($runInTransaction === true) {
                 $pdo_counter->rollback();
             }
         }
 
+
+        $failedOutput = " == <comment>Error in Migration ".$migration->getVersion()." - ".$migration->getName()."</comment>: <error>";
+
         if ($exception != null) {
+
+            $migration->getOutput()->writeln($failedOutput.$exception->getMessage().'</error>');
+
             throw $exception;
         } else if ($failedReason != null) {
+
+            $migration->getOutput()->writeln($failedOutput.$failedReason.'</error>');
+
             throw new \Exception($failedReason);
         }
 
